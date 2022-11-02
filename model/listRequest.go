b5463cef5e2943caf8c01f9cd7ae6a3b6398a7a0
Adia Mahardika <adiamahardika.work@gmail.com>
2022-11-02 16:51:25 +0700

    Add alphanumeric validation

--- model/listRequest.go
+++ model/listRequest.go
@@ -3,7 +3,7 @@ package model
 import "mime/multipart"
 
 type ListRequest struct {
-	Title       string                `json:"title" form:"title" validate:"required,max=100"`
+	Title       string                `json:"title" form:"title" validate:"required,alphanum,max=100"`
 	Description string                `json:"description" form:"description" validate:"required,max=1000"`
 	Attachment  *multipart.FileHeader `json:"attachment" form:"attachment"`
 }
